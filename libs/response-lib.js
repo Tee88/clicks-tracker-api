c859a0a5be89de378464b01f5fa5e9ab5e21f327
Taha Saleh <taha.a.saleh@gmail.com>
2021-01-30 17:50:15 -0600

    feat(root): create a response lib with a 301 response case

---
+++ libs/response-lib.js
@@ -0,0 +1,49 @@
+export function success(body) {
+    return buildResponse(200, body);
+  }
+  
+  export function failure(body) {
+    return buildResponse(500, body);
+  }
+  
+  export function badRequest(body) {
+    return buildResponse(400, body);
+  }
+  
+  export function forbidden(body) {
+    return buildResponse(403, body);
+  }
+  
+  export function notFound(body) {
+    return buildResponse(404, body);
+  }
+  
+  export function created(body) {
+    return buildResponse(201, body);
+  }
+  
+  export function conflict(body) {
+    return buildResponse(409, body);
+  }
+  
+  export function notModified(body) {
+    return buildResponse(304, body);
+  }
+
+  export function movedPermanently(body) {
+      const customHeaders = { Location: 'https://www.serverlessguru.com/'}
+    return buildResponse(301, body, customHeaders);
+  }
+  
+  function buildResponse(statusCode, body, extraHeaders = null) {
+      let defaultHeaders = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      }
+    return {
+      statusCode: statusCode,
+      headers: {...defaultHeaders, ...extraHeaders},
+      body: JSON.stringify(body),
+    };
+  }
+  

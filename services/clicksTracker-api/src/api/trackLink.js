aae4b83ddfe24069c805c4e0b619f8420383628e
Taha Saleh <taha.a.saleh@gmail.com>
2021-01-30 20:19:03 -0600

    feat(clickstracker-api): parse user agent in lambda using platform.js

--- services/clicksTracker-api/src/api/trackLink.js
+++ services/clicksTracker-api/src/api/trackLink.js
@@ -1,11 +1,22 @@
 import {movedPermanently} from "../../../../libs/response-lib"
+import platform from "platform";
 export async function main(event, context) {
     
-   
+    
     const {sourceIp, userAgent} = event.requestContext.identity
 
     console.log("IP", sourceIp)
     console.log("UA", userAgent)
+
+    const info = platform.parse(userAgent)
+    const {name: browser, version: browserVersion, product: device,description} = info
+    const os =  info.os.toString()
+
+    console.log("browser",browser)
+    console.log("browserVersion",browserVersion)
+    console.log("device",device)
+    console.log("description",description)
+    console.log("os",os)
     return movedPermanently({ success: true })
 
 }
